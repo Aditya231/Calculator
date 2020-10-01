9dc454bcb1f9ee6f01fe2a8983539d629e78abbc
Aditya <63795831+Aditya231@users.noreply.github.com>
2020-10-01 10:19:55 +0530

    improved code
    i had left 2 field for you to try out new things - check the comments ! 
    best of luck and good Calculator ! 😁😁😁

--- main.py
+++ main.py
@@ -20,18 +20,28 @@ select = int(input('Which operation do you need?1/2/3/4/5/6: '))
 
 num1 = int(input('Please enter your first number: '))
 num2 = int(input('Please enter your second number: '))
-
+# using f-string is a simple way instead of writing the variable again and again !
 if select == 1:
-  print(num1, '+', num2, '=', add(num1, num2))
+  adding = add(num1, num2)
+  print(f"{num1} + {num2} = {adding}")
+  #print(num1, '+', num2, '=', add(num1, num2))
 elif select == 2:
-  print(num1, '-', num2, '=', subtract(num1, num2))
+  subtracting = subtract (num1, num2)
+  print (f"{num1} - {num2} = {subtracting}")
+  #print(num1, '-', num2, '=', subtract(num1, num2))
 elif select == 3:
-  print(num1, 'x', num2, '=', multiply(num1, num2))
+  multiplying = multiply (num1, num2)
+  print (f"{num1} x {num2} = {multiplying}")
+  #print(num1, 'x', num2, '=', multiply(num1, num2))
 elif select == 4:
-  print(num1, '/', num2, '=' , divide(num1, num2))
+  div = divide(num1, num2)
+  print (f"{num1} / {num2} = {div}")
+  #print(num1, '/', num2, '=' , divide(num1, num2))
 elif select == 5:
+  # just do the same as above here - Try it yourself, I know you can do it !
   print(num1, '^', num2, '=', num1**num2)
 elif select == 6:
+  # F-string here also - if you have problems , type on google "f-string" !
   print(num1, 'R', num2, '=',  num1 % num2)
 else:
-  print("Invalid input!")
+   print("Invalid input!")
